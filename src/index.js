db9ce4cc5ca2d78ba17087c9e3ffc1295aa21064
Volodymyr Kovalenko <komp215.16@gmail.com>
2021-11-13 19:52:48 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './sass/main.scss';
+import './js/apiService.js';
+
+const form = document.querySelector('.form-box');
+const markupForm = `<form class="search-form" id="search-form">
+<input type="text" name="query" autocomplete="off" placeholder="Search images..." />
+</form>`
+form.insertAdjacentHTML('beforeend', markupForm)
+const input = document.querySelector('[name="query"]')
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,2 +1,8 @@
-const BASE_URL = 'https://pixabay.com/api';
-const MyKey = '24313897-08fadb9fd8bbd27a38b998116';
+const BASE_URL = 'https://pixabay.com/api'
+const Key = 'key=24313897-08fadb9fd8bbd27a38b998116'
+const page = 1
+const query = ''
+
+fetch(`${BASE_URL}api/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&${Key}`)
+    .then(response => response.json)
+    .then(data => console.log(data))
