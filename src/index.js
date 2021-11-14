cea24514d6db41559f27a04c814b8b210ea82ae3
Volodymyr Kovalenko <komp215.16@gmail.com>
2021-11-14 17:19:34 +0200

    ?

--- src/index.js
+++ src/index.js
@@ -1,8 +1,74 @@
-import './js/apiService.js';
+import apiService from './js/apiService.js';
+import template from './temp/templates.hbs';
 
 const form = document.querySelector('.form-box');
-const markupForm = `<form class="search-form" id="search-form">
-<input type="text" name="query" autocomplete="off" placeholder="Search images..." />
-</form>`
-form.insertAdjacentHTML('beforeend', markupForm)
+const gallery = document.querySelector('.gallery-list')
+const btn = document.querySelector('.btn')
+const element = document.querySelector('.gallery-box');
+
+
+markupForm()
 const input = document.querySelector('[name="query"]')
+input.addEventListener('input', debounce(searchImages, 1000))
+btn.addEventListener('click', loadMoreImage)
+
+const newApiService = new apiService()
+btn.classList.add('visibility')
+
+function searchImages(evt) {
+    evt.preventDefault()
+    gallery.innerHTML = ""
+    newApiService.page = 1
+    
+    newApiService.query = evt.target.value.trim()
+    newApiService.makeFetch().then(data => {
+        if (data.total !== 0) {
+            data.hits.map((elem) => {
+                const markup = template({ elem })
+                gallery.insertAdjacentHTML('beforeend', markup)
+                btn.classList.remove('visibility')
+                evt.target.value = ""
+            })
+        }
+        else {
+            btn.classList.add('visibility')
+            alert('Введіть коректне значення!')
+        }
+    })
+    .catch(error => console.log(error))
+}
+
+function loadMoreImage() {
+    changePage()
+    newApiService.makeFetch().then(data => {
+        data.hits.map((elem) => {
+    const markup = template({ elem })
+            gallery.insertAdjacentHTML('beforeend', markup)
+        })
+        scroll()
+    })
+}
+
+function scroll() {
+const promise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+}))
+    }, 400)
+})
+    return promise
+}
+
+function changePage() {
+    newApiService.page += 1
+}
+
+function markupForm() {
+    const markup = `<form class="search-form" id="search-form">
+    <input class= "input" type="text" name="query" autocomplete="off" placeholder="Search images..." />
+    </form>`
+    
+    form.insertAdjacentHTML('beforeend', markup)
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,8 +1,28 @@
-const BASE_URL = 'https://pixabay.com/api'
-const Key = 'key=24313897-08fadb9fd8bbd27a38b998116'
-const page = 1
-const query = ''
+export default class apiService {
+    constructor() {
+        this.BASE_URL = "https://pixabay.com/";
+        this.KEY = 'key=24313897-08fadb9fd8bbd27a38b998116'
+       this.searchQuery = "";
+        this.page = 1;
+    }
+     makeFetch() {
+    return fetch(`${this.BASE_URL}api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&${this.KEY}`)
+        .then(response => {
+        if (response.status === 200) {
+        
+          return response.json()
+                }
+        else {
+         throw new Error (response.statusText)
+        }
+    })
+    }
 
-fetch(`${BASE_URL}api/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&${Key}`)
-    .then(response => response.json)
-    .then(data => console.log(data))
+    get query() {
+        this.searchQuery
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery
+    }
+}
+
